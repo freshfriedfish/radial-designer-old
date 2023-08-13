ab406ebbdb88678b19a1019e64a0c7d9f82c7439
freshfriedfish <shafstablet@gmail.com>
2023-08-12 22:03:19 -0400

    add imports

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-console.log("bruh");
-console.log("hello bruh");
-console.log("bye bruh");
+import {Pane} from 'tweakpane';
+import {Matrix} from 'ml-matrix';
+
+const pane = new Pane({
+    title: 'Parameters',
+    expanded: true,
+});
+const PARAMS = {copies: 3, rotate_single: 0, rotate_all: 0, dist: 0, export: ""};
+
+pane.addBinding(PARAMS, 'copies', {step: 1, min: 3, max: 10,});
+pane.addBinding(PARAMS, 'rotate_single', {step: 1, min: -180, max: 180,});
+pane.addBinding(PARAMS, 'rotate_all', {step: 1, min: -180, max: 180,});
+pane.addBinding(PARAMS, 'dist', {step: 1, min: 0, max: 300,});
+pane.addBinding(PARAMS, 'export', {
+    readonly:true,
+    multiline:true,
+    rows:10,
+});
+
+const matrix = Matrix.ones(5, 5);
+
+console.log(matrix.to2DArray());
