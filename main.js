b0fc27983a4ccf4907c7986137555680bc6df1de
freshfriedfish <shafstablet@gmail.com>
2023-08-18 17:58:55 -0400

    add support for other curve types

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 import {Pane} from 'tweakpane';
 import {Matrix} from 'ml-matrix';
 
+//example shapes
+
+const straight = new Matrix([
+    [141, 331],
+    [233, 216],
+]);
+const straight3d = new Matrix([
+    [141, 331,0],
+    [233, 216,0],
+    [0,0,1]
+]);
+const quadCurve = new Matrix([
+    [150, 238, 332],
+    [122, 38, 125],
+]);
+const bezCurve = new Matrix([
+    [150, 189, 378, 420],
+    [122, 52, 53, 130],
+]);
+
 const pane = new Pane({
     title: 'Parameters',
     expanded: true,
@@ -17,55 +37,12 @@ pane.addBinding(PARAMS, 'export', {
     rows: 10,
 });
 //[[141, 233, 331, 216]]
-let straight = new Matrix([
-    [141, 331],
-    [233, 216],
-    [1,1],
-]);
-
-let straight2 = new Matrix([
-    [141, 331],
-    [233, 216],
-]);
-
-
-console.log(straight.to2DArray());
-console.log(straight.to1DArray());
-
+console.log(straight.to2DArray(), straight.to1DArray());
+console.log(quadCurve.to2DArray(), quadCurve.to1DArray());
+console.log("150, 122, 238, 38, 332, 125");
+console.log(bezCurve.to2DArray(), bezCurve.to1DArray());
+console.log("150, 122, 189, 52, 378, 53, 420, 130");
 //PARAMS.export = straight.to2DArray();
-
-window.setup = () => {
-    createCanvas(512, 384);
-    strokeWeight(20);
-    rectMode(CENTER);
-};
-
-window.draw = () => {
-    background(100);
-
-    // let move = new Matrix([
-    //     [1, 0, PARAMS.dist],
-    //     [0, 1, 0],
-    // ]);
-
-    let move2 = new Matrix([
-        [PARAMS.dist, PARAMS.dist,],
-        [0, 1]
-    ]);
-
-    let newmatrix = Matrix.add(straight2, move2);
-    //let newmatrix = move.mmul(straight);
-
-    let newmatrixarr = newmatrix.to2DArray();
-
-    console.log(newmatrix.to2DArray());
-
-    drawLine(newmatrixarr);
-
-    PARAMS.export = newmatrix.to1DArray();
-
-
-};
 
 function drawLine(arr) {
     beginShape();
@@ -74,3 +51,71 @@ function drawLine(arr) {
     endShape();
     rect(arr[0][0], arr[1][0], 10);
 }
+
+function drawCurveSingleMatrix(matrix) {
+    const marr = matrix.to2DArray();
+    beginShape();
+    switch (marr[0].length) {
+        case 2:
+            vertex(marr[0][0], marr[1][0]);
+            vertex(marr[0][1], marr[1][1]);
+            break;
+        case 3:
+            vertex(marr[0][0], marr[1][0]);
+            quadraticVertex(marr[0][1],marr[1][1],marr[0][2],marr[1][2],)
+            break;
+        case 4:
+            vertex(marr[0][0], marr[1][0]);
+            bezierVertex(marr[0][1],marr[1][1],marr[0][2],marr[1][2],marr[0][3],marr[1][3],)
+    }
+    endShape();
+    rect(marr[0][0], marr[1][0], 10);
+}
+
+//---------------------------------------------------------------------------------------------------
+
+window.setup = () => {
+    createCanvas(windowWidth, windowHeight);
+    strokeWeight(20);
+    rectMode(CENTER);
+    noFill();
+};
+window.draw = () => {
+    background(100);
+    rect(256, 192, 512, 384);
+
+    let move2 = new Matrix([
+        [PARAMS.dist, PARAMS.dist,],
+        [0, 1]
+    ]);
+
+    // let newmatrix = Matrix.add(straight, move2);
+
+
+    let x,y;
+    const centerM = new Matrix([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+    ]);
+
+    //translate via mul
+    const distM = new Matrix([
+        [1,0,PARAMS.dist],
+        [0,1,PARAMS.dist],
+        [0,0,1],
+    ]);
+
+    let newmatrix = Matrix.add(straight, move2);
+
+    drawCurveSingleMatrix(newmatrix);
+
+    push();
+    fill(0);
+    textSize(32)
+    text(newmatrix.getColumn(0), 50, 500);
+    pop();
+
+    PARAMS.export = JSON.stringify(bezCurve.to1DArray());
+
+};
