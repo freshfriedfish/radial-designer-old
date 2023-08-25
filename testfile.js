7b9e1fee1c75adcce8c50d42ccd79d4e5551d851
freshfriedfish <shafstablet@gmail.com>
2023-08-24 22:28:45 -0400

    change appearance of drawn objects

--- testfile.js
+++ testfile.js
@@ -14,6 +14,11 @@ const bezCurve = new Matrix([
     [150, 189, 378, 420],
     [122, 52, 53, 130],
 ]);
+
+const multistraight = new Matrix([
+    [192, 232, 232, 264, 264, 332],
+    [152, 152, 152, 208, 208, 208],
+]);
 /*
 TODO:
     - add support for complex slidershapes,
@@ -23,51 +28,51 @@ TODO:
 const pane = new Pane({
     title: 'radial-designer', expanded: true,
 });
-const PARAMS = {size: 4, copies: 3, rotate_single: 0, rotate_all: 0, dist: 0, center: true,
-    importType: '', importObj:'', export: ''};
+const PARAMS = {
+    size: 4, copies: 3, rotate_single: 0, rotate_all: 0, dist: 0, center: true,
+    importType: '', importObj: '', export: ''
+};
 
 ((folder) => {
     folder.addBinding(PARAMS, 'size', {step: 1, min: 1, max: 50,});
     folder.addBinding(PARAMS, 'copies', {step: 1, min: 1, max: 9,});
     folder.addBinding(PARAMS, 'rotate_all', {step: 5, min: -180, max: 180, label: 'main rotate'});
     folder.addBinding(PARAMS, 'rotate_single', {step: 5, min: -180, max: 180, label: 'sub rotate'});
-    folder.addBinding(PARAMS, 'dist', {step: 5, min: 0, max: 300,label: 'distance'});
+    folder.addBinding(PARAMS, 'dist', {step: 5, min: 0, max: 300, label: 'distance'});
     folder.addBinding(PARAMS, 'center', {label: 'center object'})
 })(pane.addFolder({
     title: 'parameters',
 }));
 ((folder) => {
-
-    folder.addBinding(PARAMS, 'importType',{
-        options:{
+    folder.addBinding(PARAMS, 'importType', {
+        options: {
             choose: '',
             svg: 'SVG',
             osu: 'OSU',
         }, label: 'import/export type',
     })
-    folder.addBinding(PARAMS, 'importObj',{
-        label:'import paste'
+    folder.addBinding(PARAMS, 'importObj', {
+        label: 'import paste'
     })
     folder.addBinding(PARAMS, 'export', {
-        readonly: true, multiline: true, rows: 20, label: null
+        readonly: true, multiline: true, rows: 8, label: null
     });
     folder.addBlade({
         view: 'separator',
     });
 })(pane.addFolder({
-    title:'import/export',
+    title: 'import/export',
 }));
 
 window.setup = () => {
     createCanvas(windowWidth, windowHeight);
     fill("rgba(0, 0, 0, 0)")
     rectMode(CENTER);
-
 };
 window.draw = () => {
     translate(width / 2, height / 2);
     //background('#0a538f');
-    background(50);
+    background(100);
     scale(2.2);
     stroke(255, 255, 255, 100);
     rect(0, 0, 512, 384);
@@ -93,14 +98,12 @@ window.draw = () => {
     const centralAngle = 360 / PARAMS.copies;
     const copyRotate = new Matrix([[Math.cos(degToRad(centralAngle)), -Math.sin(degToRad(centralAngle))], [Math.sin(degToRad(centralAngle)), Math.cos(degToRad(centralAngle))],]);
 
-
     for (let i = 0; i < PARAMS.copies; i++) {
         push();
-        strokeWeight(10)
         otherMatrices = copyRotate.mmul(otherMatrices)
         otherMatrices.round();
         allMatArr.push(otherMatrices)
-        drawCurveSingleMatrixHelper(otherMatrices);
+        drawCurveSingleMatrix(otherMatrices);
         pop();
         //PARAMS.export = PARAMS.export +'\n'+ JSON.stringify(otherMatrices.to1DArray());
         //console.log(JSON.stringify(otherMatrices.to1DArray()));
@@ -116,11 +119,22 @@ window.draw = () => {
 
 function drawCurveSingleMatrix(matrix) {
     push();
-
+    stroke(255, 200);
+    strokeWeight(60);
+    strokeCap(ROUND);
+    strokeJoin(ROUND);
+    drawCurveSingleMatrixHelper(matrix);
+    stroke(10, 83, 143, 200);
+    strokeWeight(55);
+    drawCurveSingleMatrixHelper(matrix);
     pop();
 }
 
 function drawCurveSingleMatrixHelper(matrix) {
+
+
+
+
     const marr = matrix.to2DArray();
     beginShape();
     vertex(marr[0][0], marr[1][0]);
@@ -135,7 +149,7 @@ function drawCurveSingleMatrixHelper(matrix) {
             bezierVertex(marr[0][1], marr[1][1], marr[0][2], marr[1][2], marr[0][3], marr[1][3],)
     }
     endShape();
-    rect(marr[0][0], marr[1][0], 10);
+    //rect(marr[0][0], marr[1][0], 10);
 }
 
 function degToRad(degrees) {
